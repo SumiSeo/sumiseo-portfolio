945068f414125f5e48c72108fb172820ce5d5630
Sumi <equepem@gmail.com>
2023-05-31 17:15:58 +0200

    add new work comp and css file

--- comps/Work.tsx
+++ comps/Work.tsx
@@ -1,5 +1,14 @@
+import styles from "@/styles/components/Paragraphe.module.scss";
+
 const Work = (): JSX.Element => {
-  return <div>Each Work</div>;
+  return (
+    <div className={styles.work__container}>
+      <div className={styles.work__column}></div>
+      <div className={styles.work__column}></div>
+      <div className={styles.work__column}></div>
+      <div className={styles.work__column}></div>
+    </div>
+  );
 };
 
 export default Work;
