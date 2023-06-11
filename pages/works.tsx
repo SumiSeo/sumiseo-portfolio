f44944c2b7f15f7be5fde25e1c05239c1f2019c2
Sumi <equepem@gmail.com>
2023-06-11 14:37:27 +0200

    redesign column of table and content

--- pages/works.tsx
+++ pages/works.tsx
@@ -3,10 +3,22 @@ import styles from "@/styles/Works.module.scss";
 
 const works = (): JSX.Element => {
   return (
-    <div className={styles.works}>
-      <Work year="2023" name="Fluuent" desc="example" />
-      <Work year="2022" name="Favorite medium" desc="example" />
-    </div>
+    <>
+      <div className={styles.works}>
+        <div className={styles.head}>
+          <div className={styles.text}>Year</div>
+          <div className={styles.text}>Company</div>
+          <div className={styles.text}>Projects</div>
+          <div className={styles.text}></div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <Work year="2023" name="Fluuent" desc="example" />
+          <Work year="2022" name="Favorite medium" desc="example" />
+        </div>
+      </div>
+    </>
   );
 };
 
