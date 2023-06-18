91685291ae0f24ffade9b787804697cf8743aa95
Sumi <equepem@gmail.com>
2023-06-18 16:56:27 +0200

    add footer

--- comps/Footer.tsx
+++ comps/Footer.tsx
@@ -1,7 +1,21 @@
 import styles from "@/styles/components/Footer.module.scss";
 
 const Footer = (): JSX.Element => {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <div className={styles.footer}>
+      <div className={styles.box}>
+        <span>Contact</span>
+        <span>Email</span>
+        <span>Phone</span>
+        <span>DM</span>
+      </div>
+      <div className={styles.box}>
+        <span>Watch</span>
+        <span>Github</span>
+        <span>LinkedIn</span>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
