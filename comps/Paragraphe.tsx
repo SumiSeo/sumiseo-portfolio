b4943d2bd085ba92cd88a9d0dae3ba70a0947dcb
Sumi <equepem@gmail.com>
2023-06-18 11:55:22 +0200

    change paragraph styling

--- comps/Paragraphe.tsx
+++ comps/Paragraphe.tsx
@@ -3,7 +3,8 @@ import styles from "@/styles/components/Paragraphe.module.scss";
 const Paragraphe = (): JSX.Element => {
   return (
     <div className={styles.paragraphe}>
-      Hi, I am a software engineer. <br />
+      Hi, I am <span className={styles.oblique}>SUMI</span> and I am a software
+      engineer. <br />
       <br />I am proficient in both Front-End and Back-End language using C,
       Python, TypeScript, React, GraphQL and third-party APIs.
     </div>
