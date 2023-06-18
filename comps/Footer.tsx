6a8e069893fad210075e8b8c69e10ea7a0e2f1a3
Sumi <equepem@gmail.com>
2023-06-18 22:45:46 +0200

    show personal info

--- comps/Footer.tsx
+++ comps/Footer.tsx
@@ -1,15 +1,43 @@
 import styles from "@/styles/components/Footer.module.scss";
 import ArrowIcon from "./ArrowIcon";
+import { useState } from "react";
 
 const Footer = (): JSX.Element => {
+  const [email, setEmail] = useState<boolean>(false);
+  const [phone, setPhone] = useState<boolean>(false);
+  const [instagram, setInstagram] = useState<boolean>(false);
+  const showEmail = () => {
+    setPhone(false);
+    setInstagram(false);
+    setEmail(true);
+  };
+  const showPhone = () => {
+    setPhone(true);
+    setInstagram(false);
+    setEmail(false);
+  };
+  const showInstagram = () => {
+    setPhone(false);
+    setInstagram(true);
+    setEmail(false);
+  };
   return (
     <div className={styles.footer}>
       <div className={styles.column}>
         <div className={styles.box}>
-          <span>Contact</span>
-          <span>Email</span>
-          <span>Phone</span>
-          <span>DM</span>
+          <span>Touch</span>
+          <span onClick={showEmail}>Email</span>
+          <div className={email ? styles.form : styles.hidden}>
+            equepem@gmail.com
+          </div>
+          <span onClick={showPhone}>Phone</span>
+          <div className={phone ? styles.form : styles.hidden}>
+            +33 7 67 23 49 82
+          </div>
+          <span onClick={showInstagram}>DM</span>
+          <div className={instagram ? styles.form : styles.hidden}>
+            @jerecoislesetoiles
+          </div>
         </div>
         <div className={styles.box}>
           <span>Watch</span>
