dd3972ce89082167e827db452990ceb4957d0623
Sumi <equepem@gmail.com>
2023-06-19 14:14:10 +0200

    play with media query

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,11 @@ export default function Home() {
   return (
     <>
       <div className={styles.home__container}>
+        <div className={styles.hiddenMsg}>
+          <p>Find me in the bigger screen.</p>
+          <p>좀 더 큰 화면에서 절 찾아주세요. </p>
+        </div>
+
         <div className={styles.home__column}>
           <Paragraphe />
         </div>
