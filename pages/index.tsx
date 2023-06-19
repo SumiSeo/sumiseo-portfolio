470eaf5b3118199b483d3ef89801c6c1a1567c88
Sumi <equepem@gmail.com>
2023-06-19 13:57:03 +0200

    add media queries

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,6 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.scss";
 import WorksList from "./WorksLists";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <>
